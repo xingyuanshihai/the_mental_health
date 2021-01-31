f49881f1dbd1ab91451ef82db5f5a6edb5ba3319
xingyuanshihai <xingyuanshihai@outlook.com>
2021-01-31 16:25:05 +0800

    first commit

---
+++ src/main/java/com/medical/the_mental_health/TheMentalHealthApplication.java
@@ -0,0 +1,13 @@
+package com.medical.the_mental_health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMentalHealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheMentalHealthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medical/the_mental_health/TheMentalHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.the_mental_health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheMentalHealthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
